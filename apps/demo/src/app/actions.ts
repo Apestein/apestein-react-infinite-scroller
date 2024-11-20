2e14b7325a6b565d643823934ccb27fcbe0b190b
apestein <ltn2057@protonmail.com>
2024-11-20 17:38:21 +0700

    works but needs improvement

--- apps/demo/src/app/actions.ts
+++ apps/demo/src/app/actions.ts
@@ -9,3 +9,20 @@ export async function getNextAction(cursor: number) {
     )
   return { data: range(cursor, cursor + 10, 1), nextCursor: cursor + 10 + 1 }
 }
+
+export async function getSomethingAction(
+  cursor: number,
+  isNext: boolean = true
+) {
+  await new Promise((resolve) => setTimeout(resolve, 500))
+  const range = (start: number, stop: number, step: number) =>
+    Array.from(
+      { length: (stop - start) / step + 1 },
+      (_, i) => start + i * step
+    )
+  return {
+    data: range(cursor, isNext ? cursor + 10 : cursor - 10, isNext ? 1 : -1),
+    nextCursor: cursor + 10 + 1,
+    prevCursor: cursor - 10 - 1,
+  }
+}
--- apps/demo/src/app/page.tsx
+++ apps/demo/src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client"
-import Image from "next/image"
 import { InfiniteScroller } from "@repo/ui/infinite-scroller"
-import { getNextAction } from "./actions"
+import { getSomethingAction } from "./actions"
 import { useInfiniteQuery } from "@tanstack/react-query"
 import React from "react"
 
@@ -10,15 +9,18 @@ export default function Home() {
     data,
     error,
     fetchNextPage,
+    fetchPreviousPage,
     hasNextPage,
+    hasPreviousPage,
     isFetching,
     isFetchingNextPage,
     status,
   } = useInfiniteQuery({
     queryKey: ["infinite-data"],
-    queryFn: ({ pageParam }) => getNextAction(pageParam),
+    queryFn: ({ pageParam }) => getSomethingAction(pageParam),
     initialPageParam: 0,
     getNextPageParam: (lastPage, pages) => lastPage.nextCursor,
+    getPreviousPageParam: (firstPage, pages) => firstPage.prevCursor,
   })
 
   if (status === "error") return <p>Error {error.message}</p>
@@ -27,7 +29,9 @@ export default function Home() {
     <main className="container flex min-h-screen flex-col px-4">
       <InfiniteScroller
         fetchNextPage={fetchNextPage}
+        fetchPreviousPage={fetchPreviousPage}
         hasNextPage={hasNextPage}
+        hasPreviousPage={hasPreviousPage}
         endingMessage="end"
         loadingMessage="loading..."
         className="h-60 overflow-auto"
